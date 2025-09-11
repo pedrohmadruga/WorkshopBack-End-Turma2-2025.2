1d97a6cde4db2591128680a88d72aaf51c7abbb6
PEDRO HENRIQUE FONSECA MADRUGA <pedro.henrique01@cs.unipe.edu.br>
2025-09-11 16:44:06 -0300

    feat :arrow_up: Guardando os dados da API no banco de dados

--- dia6/project/core/views.py
+++ dia6/project/core/views.py
@@ -2,11 +2,17 @@ import requests, certifi
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import PostSerializer
 
 class PostListView(APIView):
     def get(self, request):
         response = requests.get('https://jsonplaceholder.typicode.com/posts', verify=certifi.where())
         if response.status_code == 200:
             dados = response.json()
-            return Response(dados, status=status.HTTP_200_OK)
+
+            serializer = PostSerializer(data=dados, many=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response({'error': 'Não foi possível obter os dados'}, status=status.HTTP_400_BAD_REQUEST)
